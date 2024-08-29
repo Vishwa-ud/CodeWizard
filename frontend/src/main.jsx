bbf86fabbf66ee2a03746cb107af0271ebc245ed
Vinu <vinukiiamare02@gmail.com>
2024-08-30 02:29:19 +0530

    imported bootstrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
