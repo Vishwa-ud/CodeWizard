159a72cfa97845efe91064c0a7cb1ac3c20a3bf0
Vinu <vinukiiamare02@gmail.com>
2024-08-31 16:43:04 +0530

    add generate report button to panel

--- frontend/src/components/Vinuki/RightPanel.jsx
+++ frontend/src/components/Vinuki/RightPanel.jsx
@@ -10,9 +10,11 @@ function RightPanel() {
                     <button className="btn btn-success w-100 mb-2">Optimize Code</button>
                     <button className="btn btn-warning w-100 mb-2">Find Errors</button>
                     <button className="btn btn-info w-100">Refactor Code</button>
+                    <button className="btn w-100">Generate Report</button>
                 </div>
         </div>
     );
 }
 
 export default RightPanel;
+ 
