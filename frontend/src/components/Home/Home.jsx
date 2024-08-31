dd3b48a2e4974926ec7a5d5a3fba7a509374b836
Vish0111 <it21315282@my.sliit.lk>
2024-08-31 18:27:16 +0530

    feat: Update Landing page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import * as reactSpring from "@react-spring/three";
 import * as drei from "@react-three/drei";
 import * as fiber from "@react-three/fiber";
 import { useScroll, useTransform } from "framer-motion";
+import Wiz from "../../assets/wiz.png";
 
 const Home = () => {
   const { scrollY } = useScroll();
@@ -12,6 +13,7 @@ const Home = () => {
   return (
     <>
       <Header />
+
       <ShaderGradientCanvas
         importedFiber={{ ...fiber, ...drei, ...reactSpring }}
         style={{
@@ -35,23 +37,20 @@ const Home = () => {
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Before they sold out
+          <h1 className="title-font sm:text-6xl text-5xl mb-4 font-medium text-blue-100">
+              Welcome to
               <br className="hidden lg:inline-block" />
-              readymade gluten
+              &nbsp; &nbsp; &nbsp;CodeWizard
             </h1>
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
+            <p className="mb-8 leading-relaxed text-red-100 ">
+            Empower your development process with CodeWizard, the ultimate tool for automated code analysis, task management, and issue tracking. Optimize your codebase, stay on top of tasks, and resolve issues effortlessly—all in one platform.
             </p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
+              <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-9  focus:outline-none hover:bg-purple-600 rounded text-lg">
+                Get Started
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
               </button>
             </div>
           </div>
@@ -59,7 +58,7 @@ const Home = () => {
             <img
               className="object-cover object-center rounded"
               alt="hero"
-              src="https://dummyimage.com/720x600"
+              src={Wiz}
             />
           </div>
         </div>
