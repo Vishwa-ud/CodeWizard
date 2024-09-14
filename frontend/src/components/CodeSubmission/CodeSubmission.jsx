483fa8f96c3ca6869925175c191cb9326b4d8860
Nadu_Dev <inbox.nadun@gmail.com>
2024-09-14 07:48:47 +0530

    Refactor CodeSubmission component and add ShaderCanvas component
    This commit refactors the CodeSubmission component by removing unused imports and adding a new component called ShaderCanvas. The ShaderCanvas component is responsible for rendering a customized shader gradient based on the provided URL. This change enhances the functionality and visual appeal of the CodeSubmission page.

--- frontend/src/components/CodeSubmission/CodeSubmission.jsx
+++ frontend/src/components/CodeSubmission/CodeSubmission.jsx
@@ -2,11 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { motion } from "framer-motion";
 import Header from "../Home/Header";
-import { ShaderGradientCanvas, ShaderGradient } from "shadergradient";
-import * as reactSpring from "@react-spring/three";
-import * as drei from "@react-three/drei";
-import * as fiber from "@react-three/fiber";
 import FlowchartResult from "./FlowchartResult"; // Import new component
+import ShaderCanvas from "../FlowChartGen/ShaderCanvas"; // Import new component
 
 function CodeSubmission() {
   const [codeSnippet, setCodeSnippet] = useState("");
@@ -116,24 +113,7 @@ function CodeSubmission() {
   return (
     <>
       <Header p={true} />
-
-      <ShaderGradientCanvas
-        importedFiber={{ ...fiber, ...drei, ...reactSpring }}
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          zIndex: -1,
-        }}
-      >
-        <ShaderGradient
-          control="query"
-          urlString="https://www.shadergradient.co/"
-        />
-      </ShaderGradientCanvas>
-
+      <ShaderCanvas urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.7&cAzimuthAngle=180&cDistance=2.8&cPolarAngle=80&cameraZoom=9.1&color1=%23606080&color2=%238d7dca&color3=%23212121&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.15&rotationX=50&rotationY=0&rotationZ=-60&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.5&uFrequency=0&uSpeed=0.3&uStrength=1.5&uTime=8&wireframe=false" />
       <section className="text-gray-400 bg-transparent body-font relative h-screen">
         <div className="container px-5 py-8 mx-auto flex flex-col h-full">
           <div className="flex flex-col text-center w-full mb-6">
