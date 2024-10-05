f28e4d40017fc9f3b11693443b6e0c80b189d2dc
Vish0111 <it21315282@my.sliit.lk>
2024-10-05 14:55:18 +0530

    Validations Added

--- frontend/src/modals/AddEditBoardModal.jsx
+++ frontend/src/modals/AddEditBoardModal.jsx
@@ -1,141 +1,137 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import crossIcon from "../assets/icon-cross.svg";
+import boardsSlice from "../redux/boardsSlice";
 import { v4 as uuidv4 } from "uuid";
 import { useDispatch, useSelector } from "react-redux";
-import boardsSlice from "../redux/boardsSlice";
 
-function AddEditBoardModal({ setBoardModalOpen , type ,  }) {
-    const dispatch = useDispatch();
-    const [isFirstLoad, setIsFirstLoad] = useState(true);
-    const [name, setName] = useState("");
-    const [isValid, setIsValid] = useState(true);
-    const board = useSelector((state) => state.boards).find(
-      (board) => board.isActive
+function AddEditBoardModal({ setIsBoardModalOpen, type , }) {
+  const dispatch = useDispatch();
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  const [name, setName] = useState("");
+  const [newColumns, setNewColumns] = useState([
+    { name: "Todo", tasks: [], id: uuidv4() },
+    { name: "Doing", tasks: [], id: uuidv4() },
+  ]);
+  const [isValid, setIsValid] = useState(true);
+  const [nameError, setNameError] = useState("");
+  const board = useSelector((state) => state.boards).find(
+    (board) => board.isActive
+  );
+
+  if (type === "edit" && isFirstLoad) {
+    setNewColumns(
+      board.columns.map((col) => {
+        return { ...col, id: uuidv4() };
+      })
     );
+    setName(board.name);
+    setIsFirstLoad(false);
+  }
 
-
-    const [newColumns, setNewColumns] = useState([
-        { name: "Todo", tasks: [], id: uuidv4() },
-        { name: "In Progress", tasks: [], id: uuidv4() },
-    ]);
-
-    if (type === "edit" && isFirstLoad) {
-      setNewColumns(
-        board.columns.map((col) => {
-          return { ...col, id: uuidv4() };
-        })
-      );
-      setName(board.name);
-      setIsFirstLoad(false);
+  const validate = () => {
+    setIsValid(true); // Reset validity
+    setNameError(""); // Reset error message
+    if (!name.trim()) {
+      setIsValid(false);
+      setNameError("Board name is required."); // Set error message
+      return false;
     }
+    for (let i = 0 ; i < newColumns.length ; i++) {
+      if (!newColumns[i].name.trim()) {
+        return false;
+      }
+    }
+    setIsValid(true);
+    return true;
+  };
 
+  const onChange = (id, newValue) => {
+    setNewColumns((prevState) => {
+      const newState = [...prevState];
+      const column = newState.find((col) => col.id === id);
+      column.name = newValue;
+      return newState;
+    });
+  };
 
-    const onChange = (id, newValue) => {
-        setNewColumns((prevState) => {
-          const newState = [...prevState];
-          const column = newState.find((col) => col.id === id);
-          column.name = newValue;
-          return newState;
-        });
-    };
+  const onDelete = (id) => {
+    setNewColumns((prevState) => prevState.filter((el) => el.id !== id));
+  };
 
-    const onDelete = (id) => {
-        setNewColumns((prevState) => prevState.filter((el) => el.id !== id));
-    };
+  const onSubmit = (type) => {
+    setIsBoardModalOpen(false);
+    if (type === "add") {
+      dispatch(boardsSlice.actions.addBoard({ name, newColumns }));
+    } else {
+      dispatch(boardsSlice.actions.editBoard({ name, newColumns }));
+    }
+  };
 
-    const validate = () => {
-        setIsValid(false);
-        if (!name.trim()) {
-          return false;
-        }
-        for (let i = 0 ; i < newColumns.length ; i++) {
-          if (!newColumns[i].name.trim()) {
-            return false;
-          }
-        }
-        setIsValid(true);
-        return true;
-      };
-
-      const onSubmit = (type) => {
-        setBoardModalOpen(false);
-        if (type === "add") {
-          dispatch(boardsSlice.actions.addBoard({ name, newColumns }));
-        } else {
-          dispatch(boardsSlice.actions.editBoard({ name, newColumns }));
-        }
-      };
-
-   
-
-    return (
+  return (
     <div
-    className="  fixed right-0 top-0 px-2 py-4 overflow-scroll scrollbar-hide  z-50 left-0 bottom-0 justify-center items-center flex dropdown " 
-    onClick={(e) => {
+      className="  fixed right-0 top-0 px-2 py-4 overflow-scroll scrollbar-hide  z-50 left-0 bottom-0 justify-center items-center flex dropdown "
+      onClick={(e) => {
         if (e.target !== e.currentTarget) {
           return;
         }
-        setBoardModalOpen(false);
+        setIsBoardModalOpen(false);
       }}
     >
-        {/* Modal Selection */}
-        <div
+      <div
         className=" scrollbar-hide overflow-y-scroll max-h-[95vh]  bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold
        shadow-md shadow-[#364e7e1a] max-w-md mx-auto my-auto w-full px-8  py-8 rounded-xl"
-        >
-            <h3 className=" text-lg ">
-                {type === "edit" ? "Edit" : "Add New"} Board
-            </h3>
+      >
+        <h3 className=" text-lg ">
+          {type === "edit" ? "Edit" : "Add New"} Board
+        </h3>
 
-            {/* Task Name */}
+        {/* Task Name */}
 
-            <div className="mt-8 flex flex-col space-y-1">
-                <label className="  text-sm dark:text-white text-gray-500">
-                Board Name
-                </label>
-                <input
-                className=" bg-transparent  px-4 py-2 rounded-md text-sm  border-[0.5px] border-gray-600 outline-none focus:outline-[#635fc7] outline-1  ring-0  "
-                placeholder=" e.g Web Design"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                id="board-name-input"
-            />
+        <div className="mt-8 flex flex-col space-y-1">
+          <label className="  text-sm dark:text-white text-gray-500">
+            Board Name
+          </label>
+          <input
+            className=" bg-transparent  px-4 py-2 rounded-md text-sm  border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-1  ring-0  "
+            placeholder=" e.g Web Design"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="board-name-input"
+          />
+           {nameError && (
+            <p className="text-red-600 text-xs">{nameError}</p>
+          )}
+        </div>
+
+        {/* Board Columns */}
+
+        <div className="mt-8 flex flex-col space-y-3">
+          <label className=" text-sm dark:text-white text-gray-500">
+            Board Columns
+          </label>
+
+          {newColumns.map((column, index) => (
+            <div key={index} className=" flex items-center w-full ">
+              <input
+                className=" bg-transparent flex-grow px-4 py-2 rounded-md text-sm  border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-[1px]  "
+                onChange={(e) => {
+                  onChange(column.id, e.target.value);
+                }}
+                type="text"
+                value={column.name}
+              />
+              <img
+                src={crossIcon}
+                onClick={() => {
+                  onDelete(column.id);
+                }}
+                className=" m-4 cursor-pointer "
+              />
             </div>
-
-            {/* Board Columns */}
-            <div className="mt-8 flex flex-col space-y-3">
-                <label className=" text-sm dark:text-white text-gray-500">
-                    Board Columns
-                </label>
-
-                {
-                    newColumns.map((column, index) => (
-                        <div key={index} className=" flex items-center w-full ">
-                            <input
-                            className=" bg-transparent flex-grow px-4 py-2 rounded-md text-sm  border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-[1px]  "
-                            onChange={(e) => {
-                            onChange(column.id, e.target.value);
-                            }}
-                            type="text"
-                            value={column.name}
-                            />
-                            <img
-                            src={crossIcon}
-                            onClick={() => {
-                            onDelete(column.id);
-                            }}
-                            className=" m-4 cursor-pointer "
-                            />
-
-                        </div>
-                    ))
-                }
-            </div>
-
-            <div>
-
+          ))}
+          <div>
             <button
-              className=" w-full items-center hover:opacity-70 dark:text-[#635fc7] dark:bg-white  text-white bg-[#635fc7] py-2 nt-2 rounded-full "
+              className=" w-full items-center hover:opacity-70 dark:text-[#635fc7] dark:bg-white  text-white bg-[#635fc7] py-2 rounded-full "
               onClick={() => {
                 setNewColumns((state) => [
                   ...state,
@@ -154,14 +150,11 @@ function AddEditBoardModal({ setBoardModalOpen , type ,  }) {
             >
               {type === "add" ? "Create New Board" : "Save Changes"}
             </button>
-
-            </div>
-
-
+          </div>
         </div>
-        
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddEditBoardModal
+export default AddEditBoardModal;
--- frontend/src/modals/AddEditTaskModal.jsx
+++ frontend/src/modals/AddEditTaskModal.jsx
@@ -4,11 +4,12 @@ import { v4 as uuidv4 } from "uuid";
 import crossIcon from "../assets/icon-cross.svg";
 import boardsSlice from "../redux/boardsSlice";
 
-function AddEditTaskModal({ type, device, setOpenAddEditTask, setIsTaskModalOpen, taskIndex, prevColIndex = 0,}) {
+function AddEditTaskModal({ type, device, setOpenAddEditTask, setIsTaskModalOpen, taskIndex, prevColIndex = 0, }) {
     const dispatch = useDispatch();
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [isValid, setIsValid] = useState(true);
+    const [errorMessages, setErrorMessages] = useState({ title: "", subtasks: "" }); // Store error messages
 
     const board = useSelector((state) => state.boards).find(
         (board) => board.isActive
@@ -17,10 +18,9 @@ function AddEditTaskModal({ type, device, setOpenAddEditTask, setIsTaskModalOpen
     const [isFirstLoad, setIsFirstLoad] = useState(true);
     const columns = board.columns;
     const col = columns.find((col, index) => index === prevColIndex);
-    
+
     const task = col ? col.tasks.find((task, index) => index === taskIndex) : []
-    
-    
+
     const [status, setStatus] = useState(columns[prevColIndex].name);
     const [newColIndex, setNewColIndex] = useState(prevColIndex);
     const [subtasks, setSubtasks] = useState([
@@ -29,24 +29,24 @@ function AddEditTaskModal({ type, device, setOpenAddEditTask, setIsTaskModalOpen
     ]);
 
     if (type === 'edit' && isFirstLoad) {
-      setSubtasks(
-        task.subtasks.map((subtask) => {
-          return { ...subtask, id: uuidv4() };
-        })
-      )
-      setTitle(task.title)
-      setDescription(task.description)
-      setIsFirstLoad(false)
+        setSubtasks(
+            task.subtasks.map((subtask) => {
+                return { ...subtask, id: uuidv4() };
+            })
+        )
+        setTitle(task.title)
+        setDescription(task.description)
+        setIsFirstLoad(false)
     }
 
     const onChangeSubtasks = (id, newValue) => {
         setSubtasks((prevState) => {
-          const newState = [...prevState];
-          const subtask = newState.find((subtask) => subtask.id === id);
-          subtask.title = newValue;
-          return newState;
+            const newState = [...prevState];
+            const subtask = newState.find((subtask) => subtask.id === id);
+            subtask.title = newValue;
+            return newState;
         });
-      };
+    };
 
     const onDelete = (id) => {
         setSubtasks((prevState) => prevState.filter((el) => el.id !== id));
@@ -58,178 +58,174 @@ function AddEditTaskModal({ type, device, setOpenAddEditTask, setIsTaskModalOpen
     };
 
     const validate = () => {
-        setIsValid(false);
+        let valid = true;
+        const newErrorMessages = { title: "", subtasks: "" };
+
         if (!title.trim()) {
-          return false;
+            valid = false;
+            newErrorMessages.title = "Task name is required.";
         }
-        for (let i = 0; i < subtasks.length; i++) {
-          if (!subtasks[i].title.trim()) {
-            return false;
-          }
+
+        const invalidSubtasks = subtasks.filter(subtask => !subtask.title.trim());
+        if (invalidSubtasks.length > 0) {
+            valid = false;
+            newErrorMessages.subtasks = "All subtasks must have a title.";
         }
-        setIsValid(true);
-        return true;
+
+        setErrorMessages(newErrorMessages);
+        setIsValid(valid);
+        return valid;
     };
 
     const onSubmit = (type) => {
-        if (type === "add") {
-          dispatch(
-            boardsSlice.actions.addTask({
-              title,
-              description,
-              subtasks,
-              status,
-              newColIndex,
-            })
-          );
-        } else {
-          dispatch(
-            boardsSlice.actions.editTask({
-              title,
-              description,
-              subtasks,
-              status,
-              taskIndex,
-              prevColIndex,
-              newColIndex,
-            })
-          );
+        if (validate()) {
+            if (type === "add") {
+                dispatch(
+                    boardsSlice.actions.addTask({
+                        title,
+                        description,
+                        subtasks,
+                        status,
+                        newColIndex,
+                    })
+                );
+            } else {
+                dispatch(
+                    boardsSlice.actions.editTask({
+                        title,
+                        description,
+                        subtasks,
+                        status,
+                        taskIndex,
+                        prevColIndex,
+                        newColIndex,
+                    })
+                );
+            }
+            setOpenAddEditTask(false);
         }
-      };
-  
+    };
+
     return (
-    <div
-      className={
-        device === "mobile"
-          ? "  py-6 px-6 pb-40  absolute overflow-y-scroll  left-0 flex  right-0 bottom-[-100vh] top-0 dropdown "
-          : "  py-6 px-6 pb-40  absolute overflow-y-scroll  left-0 flex  right-0 bottom-0 top-0 dropdown "
-      }
-      onClick={(e) => {
-        if (e.target !== e.currentTarget) {
-          return;
-        }
-        setOpenAddEditTask(false);
-      }}
-    >
-        {/* Modal Section */}
-
         <div
-        className=" scrollbar-hide overflow-y-scroll max-h-[95vh]  my-auto  bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold
-        shadow-md shadow-[#364e7e1a] max-w-md mx-auto  w-full px-8  py-8 rounded-xl"
-        >
-            <h3 className=" text-lg ">
-            {type === "edit" ? "Edit" : "Add New"} Task
-            </h3>
-
-        {/* Task Name */}
-
-        <div className="mt-8 flex flex-col space-y-1">
-            <label className="  text-sm dark:text-white text-gray-500">
-                Task Name
-            </label>
-            <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            id="task-name-input"
-            type="text"
-            className=" bg-transparent  px-4 py-2 outline-none focus:border-0 rounded-md text-sm  border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-1  ring-0  "
-            placeholder=" e.g Take coffee break"
-          />   
-            </div>
-
-            {/* Description */}
-            <div className="mt-8 flex flex-col space-y-1">
-            <label className="  text-sm dark:text-white text-gray-500">
-                Description
-            </label>
-            <textarea
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                id="task-description-input"
-                className=" bg-transparent outline-none min-h-[200px] focus:border-0 px-4 py-2 rounded-md text-sm  border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-[1px] "
-                placeholder="e.g. It's always good to take a break. This 
-                15 minute break will  recharge the batteries 
-                a little."
-            />
-            </div>
-
-             {/* Subtasks */}
-
-            <div className="mt-8 flex flex-col space-y-3">
-            <label className="  text-sm dark:text-white text-gray-500">
-                Subtasks
-            </label>
-            
-            {subtasks.map((subtask, index) => (
-            <div key={index} className=" flex items-center w-full ">
-              <input
-                onChange={(e) => {
-                  onChangeSubtasks(subtask.id, e.target.value);
-                }}
-                type="text"
-                value={subtask.title}
-                className=" bg-transparent outline-none focus:border-0 flex-grow px-4 py-2 rounded-md text-sm  border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-[1px]  "
-                placeholder=" e.g Take coffee break"
-              />
-              <img
-                src={crossIcon}
-                onClick={() => {
-                  onDelete(subtask.id);
-                }}
-                className=" m-4 cursor-pointer "
-              />
-            </div>
-          ))}
-
-            <button
-            className=" w-full items-center dark:text-[#635fc7] dark:bg-white  text-white bg-[#635fc7] py-2 rounded-full "
-            onClick={() => {
-              setSubtasks((state) => [
-                ...state,
-                { title: "", isCompleted: false, id: uuidv4() },
-              ]);
+            className={
+                device === "mobile"
+                    ? "py-6 px-6 pb-40 absolute overflow-y-scroll left-0 flex right-0 bottom-[-100vh] top-0 dropdown "
+                    : "py-6 px-6 pb-40 absolute overflow-y-scroll left-0 flex right-0 bottom-0 top-0 dropdown "
+            }
+            onClick={(e) => {
+                if (e.target !== e.currentTarget) {
+                    return;
+                }
+                setOpenAddEditTask(false);
             }}
+        >
+            {/* Modal Section */}
+            <div
+                className="scrollbar-hide overflow-y-scroll max-h-[95vh] my-auto bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold
+                shadow-md shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl"
             >
-            + Add New Subtask
-            </button>
-            </div>
+                <h3 className="text-lg ">
+                    {type === "edit" ? "Edit" : "Add New"} Task
+                </h3>
 
-            {/* current Status  */}
-            <div className="mt-8 flex flex-col space-y-3">
-                <label className="  text-sm dark:text-white text-gray-500">
-                        Current Status
-                </label>
-                <select
-                    value={status}
-                    onChange={(e) => onChangeStatus(e)}
-                    className=" select-status flex-grow px-4 py-2 rounded-md text-sm bg-transparent focus:border-0  border-[1px] border-gray-300 focus:outline-[#635fc7] outline-none"
-                >
-                    {columns.map((column, index) => (
-                    <option value={column.name} key={index}>{column.name}</option>
+                {/* Task Name */}
+                <div className="mt-8 flex flex-col space-y-1">
+                    <label className="text-sm dark:text-white text-gray-500">
+                        Task Name
+                    </label>
+                    <input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        id="task-name-input"
+                        type="text"
+                        className={`bg-transparent px-4 py-2 outline-none focus:border-0 rounded-md text-sm border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-1 ring-0 ${!isValid && errorMessages.title ? 'border-red-500' : ''}`}
+                        placeholder="e.g Take coffee break"
+                    />
+                    {errorMessages.title && <span className="text-red-500 text-sm">{errorMessages.title}</span>}
+                </div>
+
+                {/* Description */}
+                <div className="mt-8 flex flex-col space-y-1">
+                    <label className="text-sm dark:text-white text-gray-500">
+                        Description
+                    </label>
+                    <textarea
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        id="task-description-input"
+                        className="bg-transparent outline-none min-h-[200px] focus:border-0 px-4 py-2 rounded-md text-sm border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-[1px]"
+                        placeholder="e.g. It's always good to take a break. This 15 minute break will recharge the batteries a little."
+                    />
+                </div>
+
+                {/* Subtasks */}
+                <div className="mt-8 flex flex-col space-y-3">
+                    <label className="text-sm dark:text-white text-gray-500">
+                        Subtasks
+                    </label>
+                    
+                    {subtasks.map((subtask, index) => (
+                        <div key={index} className="flex items-center w-full ">
+                            <input
+                                onChange={(e) => {
+                                    onChangeSubtasks(subtask.id, e.target.value);
+                                }}
+                                type="text"
+                                value={subtask.title}
+                                className={`bg-transparent outline-none focus:border-0 flex-grow px-4 py-2 rounded-md text-sm border-[0.5px] border-gray-600 focus:outline-[#635fc7] outline-[1px] ${!isValid && errorMessages.subtasks ? 'border-red-500' : ''}`}
+                                placeholder="e.g Take coffee break"
+                            />
+                            <img
+                                src={crossIcon}
+                                onClick={() => {
+                                    onDelete(subtask.id);
+                                }}
+                                className="m-4 cursor-pointer"
+                            />
+                        </div>
                     ))}
-                </select>
-                <button
-                    onClick={() => {
-                    const isValid = validate();
-                    if (isValid) {
-                        onSubmit(type);
-                        setOpenAddEditTask(false);
-                        type === "edit" && setOpenAddEditTask(false);
-                    }
-                    }}
-                    className=" w-full items-center text-white bg-[#635fc7] py-2 rounded-full "
-                >
-                {type === "edit" ? " save edit" : "Create task"}
-                </button>
+                    {errorMessages.subtasks && <span className="text-red-500 text-sm">{errorMessages.subtasks}</span>}
+
+                    <button
+                        className="w-full items-center dark:text-[#635fc7] dark:bg-white text-white bg-[#635fc7] py-2 rounded-full"
+                        onClick={() => {
+                            setSubtasks((state) => [
+                                ...state,
+                                { title: "", isCompleted: false, id: uuidv4() },
+                            ]);
+                        }}
+                    >
+                        + Add New Subtask
+                    </button>
+                </div>
+
+                {/* Current Status */}
+                <div className="mt-8 flex flex-col space-y-3">
+                    <label className="text-sm dark:text-white text-gray-500">
+                        Current Status
+                    </label>
+                    <select
+                        value={status}
+                        onChange={(e) => onChangeStatus(e)}
+                        className="select-status flex-grow px-4 py-2 rounded-md text-sm bg-transparent focus:border-0 border-[1px] border-gray-300 focus:outline-[#635fc7] outline-none"
+                    >
+                        {columns.map((column, index) => (
+                            <option value={column.name} key={index}>{column.name}</option>
+                        ))}
+                    </select>
+                    <button
+                        onClick={() => {
+                            onSubmit(type);
+                        }}
+                        className="w-full items-center text-white bg-[#635fc7] py-2 rounded-full"
+                    >
+                        {type === "edit" ? "Save Edit" : "Create Task"}
+                    </button>
+                </div>
             </div>
-
-
-
         </div>
-
-
-    </div>
-  )
+    )
 }
 
-export default AddEditTaskModal
+export default AddEditTaskModal;
